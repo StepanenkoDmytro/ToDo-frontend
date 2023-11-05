28ad2fcd749285775ac3bc5f560d1d40d673b8fa
Dmytro Stepanenko <dmitriy.s.stepanenko@gmail.com>
2023-11-05 20:08:42 +0200

    slice the remnants of html into components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CategoriesComponent } from './views/categories/categories.component';
 import { TasksComponent } from './views/tasks/tasks.component';
+import { FooterComponent } from './views/footer/footer.component';
+import { HeaderComponent } from './views/header/header.component';
+import { StatComponent } from './views/stat/stat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CategoriesComponent,
-    TasksComponent
+    TasksComponent,
+    FooterComponent,
+    HeaderComponent,
+    StatComponent
   ],
   imports: [
     BrowserModule,
